54e4cc3af25eb0718fe28dbc66bce3da9065e61f
Moonbear2457 <133373087+Moonbear2457@users.noreply.github.com>
2024-02-15 15:19:33 +0100

    New file

---
+++ demo.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def funciton1(x):
+    """This is a funciton that adds two numbers togehter
+
+    Parameters
+    ----------
+    a : float
+        first parameter
+    b : int
+        second parameter
+
+    Returns
+    -------
+    out : float
+        sum of a and b
+    """
+    return np.cos(x)
